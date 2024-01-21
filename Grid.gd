6f91c140e96db6f69c9fef54e184a1e32a1a4a15
akrainio <andreikrainiouk@gmail.com>
2024-01-21 11:53:08 -0700

    seperate drawing from game logic

--- Grid.gd
+++ Grid.gd
@@ -1,27 +1,12 @@
 class_name Grid
 extends Node2D
 
-
-const offset: Vector2 = Vector2(edge_len, b)
-
 const width: int = 30
 const height: int = 15
-const edge_len: int = 64
-const a: float = 0.5 * edge_len
-const b: float = edge_len * sqrt(3)/2
-const default_fill_color: Color = Color.BLACK
-const hover_fill_color: Color = Color.REBECCA_PURPLE
 
-const edge_width: float = 1.5
-const edge_color: Color = Color(1, 1, 1)
-var tiles: Array[Tile] = []
 var tile_scene: PackedScene = preload("res://Tile.tscn")
-var center_tile: Tile
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("grid ready")
 	for x: int in range(width):
 		for y: int in range(height):
 			
@@ -29,10 +14,8 @@ func _ready() -> void:
 			var tile: Tile = tile_scene.instantiate()
 			tile.init(Vector2i(x, y))
 			tile._ready()
-			tiles.append(tile)
 			self.add_child(tile)
 			
 			# check for center
 			if x == width/2 and y == height/2:
-				center_tile = tile
-				$"Camera2D".global_position = tile.center
+				$"Camera2D".global_position = tile.get_center_pos()
---
+++ TileDrawer.gd
@@ -0,0 +1,83 @@
+class_name TileDrawer
+extends Node2D
+
+# tile instance
+var tile: Tile
+
+# constants defining appearance of hex
+const default_fill_color: Color = Color.BLACK
+const hover_fill_color: Color = Color.REBECCA_PURPLE
+const edge_color: Color = Color(1, 1, 1)
+const edge_width: float = 1.5
+const edge_len: int = 64
+
+# fill color of tile instance
+var fill_color: Color = default_fill_color
+
+# math helpers
+const a: float = 0.5 * edge_len
+const b: float = edge_len * sqrt(3)/2
+
+var _center: Vector2:
+	get:
+		return Vector2(
+			tile.coordinates.x * edge_len * 1.5,
+			tile.coordinates.y * b * 2 + tile.coordinates.x%2 * b)
+
+var _right: Vector2:
+	get:
+		return Vector2(_center.x + edge_len, _center.y)
+
+var _bottom_right: Vector2:
+	get:
+		return Vector2(_center.x + a, _center.y + b)
+
+var _bottom_left: Vector2:
+	get:
+		return Vector2(_center.x - a, _center.y + b)
+
+var _left: Vector2:
+	get:
+		return Vector2(_center.x - edge_len, _center.y)
+
+var _top_left: Vector2:
+	get:
+		return Vector2(_center.x - a, _center.y - b)
+
+var _top_right: Vector2:
+	get:
+		return Vector2(_center.x + a, _center.y - b)
+
+func _init(tile_: Tile):
+	tile = tile_
+	
+func get_polygon() -> PackedVector2Array:
+	return PackedVector2Array([_right, _top_right, _top_left, _left, _bottom_left, _bottom_right, _right])
+
+func draw_hex() -> void:
+	# draw hexagon fill
+	draw_colored_polygon(
+		get_polygon(),
+		fill_color)
+	# draw hexagon edge
+	draw_polyline_colors(
+		get_polygon(),
+		PackedColorArray([edge_color, edge_color, edge_color, edge_color, edge_color, edge_color]),
+		edge_width, true)
+
+func _draw() -> void:
+	draw_hex()
+
+func _get_coordinates() -> Vector2i:
+	return $"..".coordinates
+
+func get_center() -> Vector2:
+	return _center
+
+func set_hover_color() -> void:
+	fill_color = hover_fill_color
+	queue_redraw()
+
+func reset_color() -> void:
+	fill_color = default_fill_color
+	queue_redraw()
